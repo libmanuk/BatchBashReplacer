8b111067900840aebffd3d9934405fa3f09a252f
Eric C. Weig <eweig@email.uky.edu>
2019-03-04 17:01:05 -0500

    Create bluntbashreplacer.sh

---
+++ bluntbashreplacer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Blunt Bash Replacer
+#Copyright Eric C. Weig, 2019
+
+replaceCmd(){
+
+	sed -i "s/$1/$3/g" "$2"
+
+}
+
+# sql file for element_texts
+inFile="<name of file to search in>"
+
+if grep -lq "<search_string>" $inFile ; then
+    true
+else
+    echo "<search_string> not found!  Could not use replace text <replace_string>" >> log.txt
+fi
+replaceCmd "<search_string>" "$inFile" "<replace_string>"
+shopt -s extglob
+rm !(*.txt|*.sh|*.bat|*.sql)
