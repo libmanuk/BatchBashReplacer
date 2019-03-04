ec480d8aba84b81898934497037a01fc5c2e2d53
Eric C. Weig <eweig@email.uky.edu>
2019-03-04 17:11:35 -0500

    Update bluntbashreplacer.sh

--- bluntbashreplacer.sh
+++ bluntbashreplacer.sh
@@ -2,15 +2,19 @@
 #Blunt Bash Replacer
 #Copyright Eric C. Weig, 2019
 
+# search and replace function
 replaceCmd(){
 
 	sed -i "s/$1/$3/g" "$2"
 
 }
 
-# sql file for element_texts
+# file to perform search and replace in
 inFile="<name of file to search in>"
 
+# check to see if the search string can be found in the file
+# return true and perform the desired search and replace, or
+# output unsuccessful search details to log file
 if grep -lq "<search_string>" $inFile ; then
     true
     replaceCmd "<search_string>" "$inFile" "<replace_string>"
