0d3346724b5401a0fb09ef9c3a4e7e1b36dbd2c6
Eric C. Weig <eweig@email.uky.edu>
2019-03-05 09:17:20 -0500

    Update batch_process.sh

--- sample/batch_process.sh
+++ sample/batch_process.sh
@@ -1,1 +1,7 @@
-asdf
+#!/bin/bash
+#Blunt Bash Replacer sample batch script
+#Copyright Eric C. Weig, 2019
+
+# will search and replace the word hello with the word howdy in filename.txt
+
+bash bbreplacer.sh hello howdy filename.txt
